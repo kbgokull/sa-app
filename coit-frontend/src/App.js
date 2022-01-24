d1ba98bcb0218a919df97e7a9e244adb9aacbdaa
kbgokull <gokullkb@gmail.com>
2022-01-24 22:45:02 +0530

    Modified App.js to connect to localhost:8080 instead of localhost:8080/sentiment

--- coit-frontend/src/App.js
+++ coit-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://localhost:8080', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
